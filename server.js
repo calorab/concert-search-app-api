ba26872994eb71142fe9c9631702d20bcb20f648
Caleb Gammon <calebwgammon@gmail.com>
2019-04-03 12:08:07 -0400

    server+express

--- server.js
+++ server.js
@@ -1,7 +1,67 @@
-const express = require('express');
-const app = express();
+//const express = require('express');
+
 const cors = require('cors');
 const {CLIENT_ORIGIN} = require('./config');
+//const User = require('./models/user');
+//const Investment = require('./models/investment');
+//const Portfolio = require('./models/portfolios');
+const bodyParser = require('body-parser');
+//const config = require('./config');
+const mongoose = require('mongoose');
+const moment = require('moment');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const BasicStrategy = require('passport-http').BasicStrategy;
+const https = require('https');
+const http = require('http');
+
+//var unirest = require('unirest');
+//var events = require('events');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('public'));
+
+mongoose.Promise = global.Promise;
+
+// ---------------- RUN/CLOSE SERVER -----------------------------------------------------
+let server;
+
+function runServer(urlToUse) {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(urlToUse, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(config.PORT, () => {
+                console.log(`Listening on localhost:${config.PORT}`);
+                resolve();
+            })
+                .on('error', err => {
+                mongoose.disconnect();
+                reject(err);
+            });
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer(config.DATABASE_URL).catch(err => console.error(err));
+}
+
+function closeServer() {
+    return mongoose.disconnect().then(() => new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+            if (err) {
+                return reject(err);
+            }
+            resolve();
+        });
+    }));
+}
+
 
 app.use(
     cors({

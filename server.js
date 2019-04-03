104b445200b7f4e4d58c53ed2733f0ff1392e99d
Caleb Gammon <calebwgammon@gmail.com>
2019-04-03 14:12:46 -0400

    user endpoint and tests NT

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ function closeServer() {
     }));
 }
 
-
+//----------- Built in for testing -----------
 app.use(
     cors({
         origin: CLIENT_ORIGIN
@@ -74,6 +74,129 @@ const PORT = process.env.PORT || 3000;
 app.get('/api/*', (req, res) => {
     res.json({ok: true});
 });
+
+
+// ---------------USER ENDPOINTS-------------------------------------
+// POST -----------------------------------
+// creating a new user
+app.post('/users/create', (req, res) => {
+
+    //take the name, username and the password from the ajax api call
+    let username = req.body.username;
+    let password = req.body.password;
+
+    //exclude extra spaces from the username and password
+    username = username.trim();
+    password = password.trim();
+
+    //create an encryption key
+    bcrypt.genSalt(10, (err, salt) => {
+
+        //if creating the key returns an error...
+        if (err) {
+
+            //display it
+            return res.status(500).json({
+                message: 'Internal server error'
+            });
+        }
+
+        //using the encryption key above generate an encrypted pasword
+        bcrypt.hash(password, salt, (err, hash) => {
+
+            //if creating the ncrypted pasword returns an error..
+            if (err) {
+
+                //display it
+                return res.status(500).json({
+                    message: 'Internal server error'
+                });
+            }
+
+            //using the mongoose DB schema, connect to the database and create the new user
+            User.create({
+                username,
+                password: hash,
+            }, (err, item) => {
+
+                //if creating a new user in the DB returns an error..
+                if (err) {
+                    //display it
+                    return res.status(500).json({
+                        message: 'Internal Server Error'
+                    });
+                }
+                //if creating a new user in the DB is succefull
+                if (item) {
+
+                    //display the new user
+                    console.log(`User \`${username}\` created.`);
+                    return res.json(item);
+                }
+            });
+        });
+    });
+});
+
+// signing in a user
+app.post('/users/login', function (req, res) {
+
+    //take the username and the password from the ajax api call
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //using the mongoose DB schema, connect to the database and the user with the same username as above
+    User.findOne({
+        username: username
+    }, function (err, items) {
+
+        //if the there is an error connecting to the DB
+        if (err) {
+
+            //display it
+            return res.status(500).json({
+                message: "Internal server error"
+            });
+        }
+        // if there are no users with that username
+        if (!items) {
+            //display it
+            return res.status(401).json({
+                message: "Not found!"
+            });
+        }
+        //if the username is found
+        else {
+
+            //try to validate the password
+            items.validatePassword(password, function (err, isValid) {
+
+                //if the connection to the DB to validate the password is not working
+                if (err) {
+
+                    //display error
+                    console.log('Could not connect to the DB to validate the password.');
+                }
+
+                //if the password is not valid
+                if (!isValid) {
+
+                    //display error
+                    return res.status(401).json({
+                        message: "Password Invalid"
+                    });
+                }
+                //if the password is valid
+                else {
+                    //return the logged in user
+                    console.log(`User \`${username}\` logged in.`);
+                    return res.json(items);
+                }
+            });
+        };
+    });
+});
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
--- test/test-server.js
+++ test/test-server.js
@@ -1,7 +1,81 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+const moment = require('moment');
 
-const {app} = require('../server');
+// -------set up model fro Users-------
+const User = require('../models/user');
+const {app, runServer, closeServer} = require('../server');
+const {
+    TEST_DATABASE_URL
+} = require('../config');
+
+function generateUser() {
+    return {
+        username: faker.internet.email(),
+        password: faker.internet.password()
+    }
+}
+
+function seedUserData() {
+    console.info('Seeding user data');
+    const seedData = [];
+
+    for (let i = 1; i < 10; i++) {
+        seedData.push(generateUser());
+    }
+    return User.insertMany(seedData);
+}
+
+
+// --------------- Test User Endpoints ---------------
+
+describe('User API resource', function () {
+
+    before(function () {
+        return runServer(TEST_DATABASE_URL)
+            .then(console.log('Running server'))
+            .catch(err => console.log({
+            err
+        }));
+    });
+
+    beforeEach(function () {
+        return seedUserData();
+    });
+
+    // Test create a new user
+    it('should create a new user', function () {
+        const newUser = generateUser();
+        return chai.request(app)
+            .post('/users/create')
+            .send(newUser)
+            .then(function (res) {
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.include.keys('username', 'password');
+            res.body.username.should.equal(newUser.username);
+            res.body.password.should.not.equal(newUser.password);
+            res.body._id.should.not.be.null;
+        });
+    });
+
+
+    afterEach(function () {
+        return tearDownDb();
+    });
+
+    after(function () {
+        return closeServer();
+    });
+});
+
+
+// -------set up config.js with test database url-------
+const {
+    TEST_DATABASE_URL
+} = require('../config');
 
 const should = chai.should();
 chai.use(chaiHttp);
